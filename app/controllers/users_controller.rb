d047616d8f7ce6f6dd5068dd8a85eb9798fd8b51
Moe <moe_akahane@diveintocode.jp>
2020-08-07 14:02:58 +0900

    セッションページ実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -10,6 +11,7 @@ class UsersController < ApplicationController
       render:new
     end
   end
+
   def show
     @user = User.find(params[:id])
   end
