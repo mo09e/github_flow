a55c83f371207427001ccf3b1977ea984fa720d0
Moe <moe_akahane@diveintocode.jp>
2020-08-10 20:44:36 +0900

    修正のためのコメントアウト削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   resources :blogs do
     collection do
       post :confirm
-      # patch :confirm
-      # patch '/blogs', to: 'blogs#confirm'
     end
   end
   resources :users, only: [:new, :create, :show]
